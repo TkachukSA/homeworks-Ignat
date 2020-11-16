dcb5c9aebd373f960b29439c75e003a3c84bb5bc
asa121asa <crafta.net@gmail.com>
2020-11-16 16:31:08 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homeworks-Ignat/precache-manifest.27f792100789856af98c6b825d64f594.js"
+  "/homeworks-Ignat/precache-manifest.2d98163f536997552e9b5bb543831732.js"
 );
 
 self.addEventListener('message', (event) => {
