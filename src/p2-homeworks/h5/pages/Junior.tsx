7211d907864f62dd3b16a11490bf7bd46ee7eb4c
asa121asa <crafta.net@gmail.com>
2020-11-16 03:36:05 +0300

    upd

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Junior() {
+    return (
+        <div>
+           Hi Junior
+        </div>
+    );
+}
+
+export default Junior;
---
+++ src/p2-homeworks/h5/pages/Testspage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Testspage() {
+    return (
+        <div>
+           my page
+        </div>
+    );
+}
+
+export default Testspage;
